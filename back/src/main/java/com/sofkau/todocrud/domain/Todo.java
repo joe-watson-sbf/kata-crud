309af9b8966ee4cb0bcbacb85d7174ed3e7370e6
Joe Watson SBF <joewatson40@hotmail.com>
2021-09-15 23:27:16 -0500

    Repositorio implementado

--- back/src/main/java/com/sofkau/todocrud/domain/Todo.java
+++ back/src/main/java/com/sofkau/todocrud/domain/Todo.java
@@ -7,7 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+/**
+ * El modelo
+ * */
 @Data
 @Entity
 @NoArgsConstructor
