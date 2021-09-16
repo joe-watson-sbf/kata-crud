36f3b693daf963614eb513e0bbca7c442385232e
Joe Watson SBF <joewatson40@hotmail.com>
2021-09-15 23:22:33 -0500

    first commit

---
+++ back/src/main/java/com/sofkau/todocrud/domain/Todo.java
@@ -0,0 +1,20 @@
+package com.sofkau.todocrud.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private boolean isCompleted;
+}
